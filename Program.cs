7ac1a7bca7a55684dd3d67ae2703e4188d6e76d3
DarkinTran <darkintran2005@gmail.com>
2025-07-28 10:43:12 +0700

    fix: correct connection string name in Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ShoeStoreDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("tule")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<JwtService>();
 
 // Configure JWT Authentication
